5d31b7f01c75f9e9271e4b4aed2aa26a1a67fc6b
cantwt <cantwt22@gmail.com>
2013-05-02 20:45:16 -0400

    added serialport initial support. Super sketchy, I need to learn how to use it.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 scown
 =====
 
-node-webkit app for Open Sound Control Automation
+node-webkit app for Open Sound Control Automation  
+Note: This app is being used by the author to learn basic web technologies.  
+It's quite rough, but it does work. Not production ready.
+
---
+++ js/main.js
@@ -0,0 +1,102 @@
+var gui = require('nw.gui');
+var osc = require('node-osc');
+var menu = new gui.Menu();
+
+var client = new osc.Client('127.0.0.1', 12346);
+
+var oscServer = new osc.Server(12346, '0.0.0.0');
+oscServer.on("/drop", function (msg, rinfo) {
+      var element = document.createElement('div');
+      element.appendChild(document.createTextNode(msg[0] + ', ' + msg[1]));
+      document.body.appendChild(element);
+});
+oscServer.on("/test", function (msg, rinfo) {
+    console.log(msg);
+});
+
+var SerialPort = require('serialport').SerialPort;
+var serial0 = new SerialPort("/dev/ttyUSB0", {
+  baudrate: 9600
+});
+
+var serial1 = new SerialPort("/dev/ttyUSB1", {
+  baudrate: 9600
+});
+
+serial0.on("open", function () {
+  console.log('open');
+  serial0.on('data', function(data) {
+    console.log('data received: ' + data);
+  });  
+  serial0.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });  
+});
+
+serial1.on("open", function () {
+  console.log('open');
+  serial1.on('data', function(data) {
+    console.log('data received: ' + data);
+  });  
+  serial1.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });  
+}); 
+
+menu.append(new gui.MenuItem({
+  label: 'Click Me',
+  click: function() {
+    var element = document.createElement('div');
+    element.appendChild(document.createTextNode('Clicked OK'));
+    document.body.appendChild(element);
+    client.send('/drop', 200);
+  }
+}));
+
+var handleClickA = function (event) {
+  client.send('/drop', 100);
+};
+
+var handleClickB = function (event) {
+  client.send('/drop', 200);
+};
+
+var handleClickC = function (event) {
+  serial0.write("Sending from 0");
+};
+
+var handleClickD = function (event) {
+  serial1.write("Sending from 1");
+};
+
+var buttonA = document.getElementById('a');
+var buttonB = document.getElementById('b');
+var buttonC = document.getElementById('c');
+var buttonD = document.getElementById('d');
+buttonA.addEventListener('click', handleClickA);
+buttonB.addEventListener('click', handleClickB);
+buttonC.addEventListener('click', handleClickC);
+buttonD.addEventListener('click', handleClickD);
+
+$(function() {
+  $( '#master' ).slider({
+    value: 60,
+    orientation: "horizontal",
+    range: "min",
+    animate: true
+  });
+  $( "#sliders > span").each(function() {
+    var value = parseInt( $( this ).text(), 10);
+    $( this ).empty().slider({
+      value: value,
+      orientation: "vertical",
+      range: "min",
+      animate: true,
+      slide: function( event, ui ) {
+        client.send('/test', ui.value);
+      }
+    });
+  });
+});
